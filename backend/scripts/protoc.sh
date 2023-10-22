2a024930397811b3f15061187d9196dd951b99db
AoiSatonaka <shonan.0821@gmail.com>
2023-10-22 21:46:32 +0900

    feat(backend): protocによるコード自動生成をDockerを用いる形で実装

---
+++ backend/scripts/protoc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+proto_file_dir='../api'
+api_proto_files=$(find ${proto_file_dir} -type f -name '*.proto')
+proto_out_dir='./grpc'
+
+# change working directory
+cd $(dirname $0)
+cd ..
+
+# out dir exists check
+if [ ! -d $proto_out_dir ]; then
+	mkdir $proto_out_dir
+fi
+
+# protobuf build
+protoc \
+	-I $proto_file_dir \
+	--go_out=$proto_out_dir \
+	--go_opt=paths=source_relative \
+	--go-grpc_out=$proto_out_dir \
+	--go-grpc_opt=paths=source_relative \
+	$api_proto_files
