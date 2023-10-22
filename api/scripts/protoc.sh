5ddaf2fde8bba620bf56b03fc56a1001b8e21784
AoiSatonaka <shonan.0821@gmail.com>
2023-10-22 20:42:00 +0900

    feat(api): protocの実行環境をDockerへと移行

---
+++ api/scripts/protoc.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+proto_file_dir='.'
+api_proto_files=$(find ${proto_file_dir} -type f -name '*.proto')
+proto_out_dir='../docs'
+proto_out_type='markdown'
+proto_out_file='protobuf.md'
+
+# change working directory
+cd $(dirname $0)
+cd ..
+
+# out dir exists check
+if [ ! -d $proto_out_dir ]; then
+	mkdir $proto_out_dir
+fi
+
+# protobuf build
+protoc \
+	--doc_out=$proto_out_dir \
+	--doc_opt="${proto_out_type},${proto_out_file}" \
+	$api_proto_files
